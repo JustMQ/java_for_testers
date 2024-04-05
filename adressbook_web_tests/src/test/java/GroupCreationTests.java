3fa553a1d7c5a9f47205fa06f98774796ae2f587
Владимир Киселев <justmq@gmail.com>
2024-04-05 17:30:14 +0300

    Лекция 2.7. Повторение пройденного материала
    Добавлен кейс на удаление групп с проверкой на наличие групп для удаления.

--- adressbook_web_tests/src/test/java/GroupCreationTests.java
+++ adressbook_web_tests/src/test/java/GroupCreationTests.java
@@ -31,7 +31,7 @@ public class GroupCreationTests {
 
     @Test
     public void canCreateGroup() {
-        if(! isElementPresent(By.name("new"))) {
+        if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
         driver.findElement(By.name("new")).click();
@@ -55,19 +55,19 @@ public class GroupCreationTests {
     }
 
     @Test
-        public void canCreateGroupWithEmptyName() {
-        if(! isElementPresent(By.name("new"))) {
+    public void canCreateGroupWithEmptyName() {
+        if (!isElementPresent(By.name("new"))) {
             driver.findElement(By.linkText("groups")).click();
         }
-            driver.findElement(By.linkText("groups")).click();
-            driver.findElement(By.name("new")).click();
-            driver.findElement(By.name("group_name")).click();
-            driver.findElement(By.name("group_name")).sendKeys("");
-            driver.findElement(By.name("group_header")).click();
-            driver.findElement(By.name("group_header")).sendKeys("");
-            driver.findElement(By.name("group_footer")).click();
-            driver.findElement(By.name("group_footer")).sendKeys("");
-            driver.findElement(By.name("submit")).click();
-            driver.findElement(By.linkText("group page")).click();
-        }
+        driver.findElement(By.linkText("groups")).click();
+        driver.findElement(By.name("new")).click();
+        driver.findElement(By.name("group_name")).click();
+        driver.findElement(By.name("group_name")).sendKeys("");
+        driver.findElement(By.name("group_header")).click();
+        driver.findElement(By.name("group_header")).sendKeys("");
+        driver.findElement(By.name("group_footer")).click();
+        driver.findElement(By.name("group_footer")).sendKeys("");
+        driver.findElement(By.name("submit")).click();
+        driver.findElement(By.linkText("group page")).click();
+    }
 }
