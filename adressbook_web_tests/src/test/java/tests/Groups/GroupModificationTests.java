bc31f47babc26aa61264650147286e3fda02b4d3
Владимир Киселев <justmq@gmail.com>
2024-04-19 20:04:42 +0300

    Лекция 4.5. Параметризованные тесты

--- adressbook_web_tests/src/test/java/tests/Groups/GroupModificationTests.java
+++ adressbook_web_tests/src/test/java/tests/Groups/GroupModificationTests.java
@@ -1,7 +1,6 @@
 package tests.Groups;
 
 import model.GroupData;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
 
@@ -13,15 +12,5 @@ public class GroupModificationTests extends TestBase {
             app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
         }
         app.groups().modifyGroup(new GroupData().withName("modified name"));
-    }
-
-    @Test
-    void canRemoveAllGroupsAtOnce() {
-        if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData("group name", "group header", "group footer"));
-        }
-        app.groups().removeAllGroups();
-        Assertions.assertEquals(0, app.groups().getCount());
-
     }
 }
