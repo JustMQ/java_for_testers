9837fecf88348b241fdefcfc2035b6204e99a02c
Владимир Киселев <justmq@gmail.com>
2024-04-25 22:19:56 +0300

    Лекция 5.3. Генератор тестовых данных, анализ опций командной строки

--- adressbook_web_tests/src/test/java/tests/Contacts/ContactCreationTests.java
+++ adressbook_web_tests/src/test/java/tests/Contacts/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package tests.Contacts;
 
+import common.CommonFunctions;
 import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -24,8 +25,8 @@ public class ContactCreationTests extends TestBase {
                                                             }
         for(int i = 0; i < 5; i++) {
             result.add(new ContactData()
-                            .withFirstName(randomString(i * 10))
-                            .withLastName(randomString(i * 10))
+                            .withFirstName(CommonFunctions.randomString(i * 10))
+                            .withLastName(CommonFunctions.randomString(i * 10))
                 );
         }
         return result;
@@ -50,8 +51,8 @@ public class ContactCreationTests extends TestBase {
     @Test
     void canCreateContact() {
         var contact = new ContactData()
-                .withFirstName(randomString(10))
-                .withLastName(randomString(10))
+                .withFirstName(CommonFunctions.randomString(10))
+                .withLastName(CommonFunctions.randomString(10))
                 .withPhoto(randomFile("src/test/resources/images"));
         app.contacts().createContact(contact);
     }
