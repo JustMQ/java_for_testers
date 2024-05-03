2329c1f21533dfa04e170dc9bc7bc9da86383c85
Владимир Киселев <justmq@gmail.com>
2024-05-03 10:36:36 +0300

    Лекция 6.3. ORM: создание и изменение записей в базе данных

--- adressbook_web_tests/src/test/java/tests/Groups/GroupRemovalTests.java
+++ adressbook_web_tests/src/test/java/tests/Groups/GroupRemovalTests.java
@@ -12,18 +12,18 @@ public class GroupRemovalTests extends TestBase {
 
     @Test
     public void canRemoveGroup() {
-        if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData(
+        if (app.hbm().getGroupCount() == 0) {
+            app.hbm().createGroup(new GroupData(
                     "",
                     properties.getProperty("web.groupname"),
                     properties.getProperty("web.groupheader"),
                     properties.getProperty("web.groupfooter")));
         }
-        var oldGroups = app.groups().getList();
+        var oldGroups = app.hbm().getGroupList();
         var rnd = new Random();
         var index = rnd.nextInt(oldGroups.size());
         app.groups().removeGroup(oldGroups.get(index));
-        var newGroups = app.groups().getList();
+        var newGroups = app.hbm().getGroupList();
         var expectedList = new ArrayList<>(oldGroups);
         expectedList.remove(index);
         Assertions.assertEquals(newGroups, expectedList);
@@ -31,15 +31,15 @@ public class GroupRemovalTests extends TestBase {
 
     @Test
     void canRemoveAllGroupsAtOnce() {
-        if (app.groups().getCount() == 0) {
-            app.groups().createGroup(new GroupData(
+        if (app.hbm().getGroupCount() == 0) {
+            app.hbm().createGroup(new GroupData(
                     "",
                     properties.getProperty("web.groupname"),
                     properties.getProperty("web.groupheader"),
                     properties.getProperty("web.groupfooter")));
         }
         app.groups().removeAllGroups();
-        Assertions.assertEquals(0, app.groups().getCount());
+        Assertions.assertEquals(0, app.hbm().getGroupCount());
 
     }
 }
