b1827cdfef9560982012631d1a7f97d5d86eeac3
Владимир Киселев <justmq@gmail.com>
2024-05-10 18:06:13 +0300

    Лекция 7.6. Словари, или ассоциативные массивы (Map)

--- adressbook_web_tests/src/test/java/tests/Contacts/ContactInfoTests.java
+++ adressbook_web_tests/src/test/java/tests/Contacts/ContactInfoTests.java
@@ -1,5 +1,6 @@
 package tests.Contacts;
 
+import model.ContactData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import tests.TestBase;
@@ -12,11 +13,12 @@ public class ContactInfoTests extends TestBase {
     @Test
     void TestPhones() {
         var contacts = app.hbm().getContactList();
-        var contact = contacts.get(0);
-        var phones = app.contacts().getPhones(contact);
-        var expected = Stream.of(contact.home(), contact.mobile(), contact.work(),  contact.secondary())
-                .filter(s -> s !=null && ! "".equals(s))
-                .collect(Collectors.joining("\n"));
+        var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
+            Stream.of(contact.home(), contact.mobile(), contact.work(), contact.secondary())
+                    .filter(s -> s != null && !"".equals(s))
+                    .collect(Collectors.joining("\n"))
+        ));
+        var phones = app.contacts().getPhones();
         Assertions.assertEquals(expected, phones);
     }
 }
